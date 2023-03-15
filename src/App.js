b25c722312812329b9c5c8ef710ab7393528356c
Ligita <legyta@gmail.com>
2023-03-15 14:22:59 +0100

    site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import WomenSessions from "./WomenSessions/WomenSessions";
 import Art from "./Art/Art";
@@ -10,14 +10,16 @@ import Contact from "./Contact/Contact";
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path exact="./home" component={HomePage} />
-        <Route path="/women-sessions" component={WomenSessions} />
-        <Route path="/art" component={Art} />
-        <Route path="/writings" component={Writings} />
-        <Route path="/reiki" component={Reiki} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route path exact="./home" component={HomePage} />
+          <Route path="/women-sessions" component={WomenSessions} />
+          <Route path="/art" component={Art} />
+          <Route path="/writings" component={Writings} />
+          <Route path="/reiki" component={Reiki} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
