543425ddeb26dc66608a198f33cbdc7d022abcf9
Ligita <legyta@gmail.com>
2023-04-30 14:25:29 +0200

    test

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import Reiki from "./Reiki/Reiki";
 import Contact from "./Contact/Contact";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter>
     <Routes>
       <Route index path="/" element={<HomePage />} />
-      <Route path="/women-sessions" element={<WomenSessions />} />
+      {/* <Route path="/women-sessions" element={<WomenSessions />} />
       <Route path="/art" element={<Art />} />
       <Route path="/writings" element={<Writings />} />
       <Route path="/reiki" element={<Reiki />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/contact" element={<Contact />} /> */}
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
